8595e60f5c1223b6bfc600579c45e6e23b2435f4
Smarsden007 <seannmarsden@gmail.com>
2022-04-27 18:18:01 -0700

    Removing, adding and editing all works as intended,

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ import Movie from './components/Movie';
 import MovieHeader from './components/MovieHeader';
 
 import FavoriteMovieList from './components/FavoriteMovieList';
-
+import AddMovieForm from "./components/AddMovieForm";
+import EditMovieForm from "./components/EditMovieForm";
 import axios from 'axios';
+
+
 
 const App = (props) => {
   const [movies, setMovies] = useState([]);
@@ -25,6 +28,7 @@ const App = (props) => {
   }, []);
 
   const deleteMovie = (id)=> {
+    setMovies(movies.filter(item=>(item.id !== Number(id))))
   }
 
   const addToFavorites = (movie) => {
@@ -44,10 +48,13 @@ const App = (props) => {
         
           <Switch>
             <Route path="/movies/edit/:id">
+              <EditMovieForm setMovies={setMovies} />
             </Route>
-
+            <Route path="/movies/add">
+              <AddMovieForm />
+            </Route>
             <Route path="/movies/:id">
-              <Movie/>
+              <Movie deleteMovie={deleteMovie} />
             </Route>
 
             <Route path="/movies">
